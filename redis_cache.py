95cc1fae96b945e0e9e1ba32c2a2a532411dbd17
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2022-08-23 15:13:56 +0400

    add redis cache

---
+++ redis_cache.py
@@ -0,0 +1,33 @@
+import redis
+import os
+from typing import Any
+
+
+class RedisCache:
+
+    def __init__(self) -> None:
+        self.r: redis.Redis = None
+
+    def connect(self):
+        if self.r != None:
+            self.r.close()
+        self.r = redis.Redis(
+            host=os.environ['REDIS_SERVICE'],
+            port=os.environ['REDIS_PORT'],
+            password='',
+            decode_responses=True,
+        )
+
+    def set(self, key: str, value: Any):
+        if self.r is None:
+            self.connect()
+        self.r.set(key, value)
+
+    def cache(self, db, key: str, val: str) -> None:
+        key = f'{os.environ[db]}:{key}'
+        self.set(key, val)
+
+    def get(self, key: str) -> str:
+        if self.r is None:
+            self.connect()
+        return self.r.get(key)
