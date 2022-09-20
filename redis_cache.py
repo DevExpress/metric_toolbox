2f03481449da05b87709893682f0d06547972ae1
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2022-09-20 18:09:03 +0400

    use one shared instance

--- redis_cache.py
+++ redis_cache.py
@@ -6,18 +6,20 @@ from typing import Any, Optional
 class RedisCache:
 
     def __init__(self, db_name: str = None) -> None:
-        self.db_name = db_name or os.environ['REDIS_DB']
-        self.r: redis.Redis = None
+        self.db_name = db_name
 
-    def connect(self):
-        if self.r != None:
-            self.r.close()
-        self.r = redis.Redis(
-            host=os.environ['REDIS_SERVICE'],
-            port=os.environ['REDIS_PORT'],
-            password='',
-            decode_responses=True,
-        )
+    def ensure_connection(self):
+        if not hasattr(RedisCache, 'redis_instance'):
+            RedisCache.redis_instance = redis.Redis(
+                host=os.environ['REDIS_SERVICE'],
+                port=os.environ['REDIS_PORT'],
+                password='',
+                decode_responses=True,
+            )
+
+    def get(self, key: str) -> str:
+        self.ensure_connection()
+        return RedisCache.redis_instance.get(key)
 
     def set(
         self,
@@ -25,30 +27,15 @@ class RedisCache:
         value: Any,
         ex: Optional[int],
     ):
-        if self.r is None:
-            self.connect()
-        self.r.set(
+        self.ensure_connection()
+        RedisCache.redis_instance.set(
             key,
             value,
             ex=ex,
         )
 
-    def cache(
-        self,
-        key: str,
-        val: str,
-        ex: Optional[int] = None,
-    ) -> None:
-        self.set(
-            key=key,
-            value=val,
-            ex=ex,
-        )
-
-    def get(self, key: str) -> str:
-        if self.r is None:
-            self.connect()
-        return self.r.get(key)
-
     def _get_key(self, *kargs) -> str:
-        return f'{self.db_name}:{":".join(str(arg) for arg in kargs)}'
+        return f'{self._get_db_name()}:{":".join(str(arg) for arg in kargs)}'
+
+    def _get_db_name(self):
+        return self.db_name or os.environ['REDIS_DB']
