c2f393d17879138cfeb262ba8fac2796378c36ee
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2022-10-11 15:19:32 +0400

    add query getter

--- utils/query_gettter.py
+++ utils/query_gettter.py
@@ -1,4 +1,4 @@
-from typing import List, Type
+from typing import List, Dict, Type
 from pathlib import Path
 
 from toolbox.sql.sql_query import SqlQuery
@@ -8,11 +8,11 @@ class QueryGetter:
 
     @staticmethod
     def get_queries_in_dir(
-        self,
         sql_query_type: Type[SqlQuery],
+        format_params: Dict[str, str], 
         dir_path: str,
     ) -> List[SqlQuery]:
         return [
-            sql_query_type(query_file_path=path, format_params={})
+            sql_query_type(query_file_path=path, format_params=format_params)
             for path in Path(dir_path).iterdir() if path.is_file()
         ]
