dfe4dcf08730b7ba6136c63f17e7389e404ad9c9
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2022-10-13 19:19:16 +0400

    update

--- utils/df_json_converter.py
+++ utils/df_json_converter.py
@@ -1,4 +1,5 @@
-from pandas import DataFrame, read_json
+from typing import List
+from pandas import DataFrame, read_json, to_datetime
 
 
 class DF_to_JSON:
@@ -14,10 +15,24 @@ class DF_to_JSON:
 class JSON_to_DF:
 
     @staticmethod
-    def convert(json: str) -> DataFrame:
+    def convert(json: str, dt_cols: List[str] = []) -> DataFrame:
         if not json:
             raise ValueError('Empty response')
-        return read_json(
+
+        df = read_json(
             json,
             orient='records',
         )
+
+        JSON_to_DF._convert_dt_columns(df, dt_cols)
+
+        return df
+
+    @staticmethod
+    def _convert_dt_columns(df: DataFrame, cols: List[str]):
+        for col in cols:
+            if col in df.columns:
+                df[col] = to_datetime(
+                    df[col],
+                    utc=True,
+                )
