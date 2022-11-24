3962fa7b57614e90c6d4702b37988636727e841a
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2022-11-24 11:37:59 +0400

    add test

---
+++ sql/Tests/test_base_repository.py
@@ -0,0 +1,43 @@
+import pytest
+from typing import Dict, Any
+from pandas import DataFrame
+
+from toolbox.sql.base_repository import BaseRepository
+from toolbox.sql.sql_query import SqlQuery
+from toolbox.sql.query_executors.sqlite_query_executor import SQLiteQueryExecutor
+from toolbox.sql.columns_validator import InvalidDataFormatException
+
+
+def test_raise_exception_if_data_does_not_contain_required_columns():
+    """
+    GIVEN DataFrame with missing required column names
+    WHEN obtained from the TribesRepository
+    THEN it should raise InvalidDataFormatException
+    """
+
+    with pytest.MonkeyPatch.context() as monkeypatch:
+
+        def mock_execute(
+            sql_query: SqlQuery,
+            kargs: Dict[str, Any] = None,
+        ):
+            data = {'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']}
+            return DataFrame(data=data)
+
+        monkeypatch.setattr(
+            SQLiteQueryExecutor,
+            'execute',
+            mock_execute,
+        )
+        columns = sorted(['asd', 'qwe'])
+        with pytest.raises(InvalidDataFormatException) as exec_info:
+            BaseRepository(
+                sql_query_type=SqlQuery,
+                query_executor=SQLiteQueryExecutor,
+            ).get_data(
+                query_file_path='',
+                query_format_params='query_format_params',
+                must_have_columns=columns,
+            )
+        columns_str = ', '.join(columns)
+        assert exec_info.value.message == f'DataFrame must contain ({columns_str}) but got (col_1, col_2)'
