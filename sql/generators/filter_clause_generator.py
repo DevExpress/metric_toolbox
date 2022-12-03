4229138dcc0a17419541abda312738cd73de0385
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2022-12-03 20:29:16 +0400

    addd sql filter clause generator

---
+++ sql/generators/filter_clause_generator.py
@@ -0,0 +1,50 @@
+from typing import Callable, Any
+
+
+class SqlFilterClauseGenerator:
+
+    def generate_in_filter(
+        self,
+        col: str,
+        values: list,
+        filter_prefix: str,
+        values_converter: Callable[[Any], str],
+    ) -> str:
+
+        def filter_func():
+            res = f'{col} IN ('
+            res += ','.join([values_converter(val) for val in values])
+            res += ')'
+            return res
+
+        return self._generate_filter(
+            values=values, filter_prefix=filter_prefix, get_filter=filter_func
+        )
+
+    def generate_like_filter(
+        self,
+        col: str,
+        values: list,
+        filter_prefix: str,
+    ):
+
+        def filter_func():
+            res = '('
+            res += ' OR '.join([f"{col} LIKE '%{value}%'" for value in values])
+            res += ')'
+            return res
+
+        return self._generate_filter(
+            values=values, filter_prefix=filter_prefix, get_filter=filter_func
+        )
+
+    def _generate_filter(
+        self,
+        values: list,
+        filter_prefix: str,
+        get_filter: Callable[[], str],
+    ) -> str:
+        if not values:
+            return ''
+        actual_filter = get_filter()
+        return filter_prefix + actual_filter
