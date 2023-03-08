aa0b3e361940e0a0145e3bcfdb53e41383a28116
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-03-08 13:35:31 +0400

    add decorator factory

--- sql/generators/filter_clause_generator.py
+++ sql/generators/filter_clause_generator.py
@@ -1,55 +1,44 @@
 from typing import Callable, Any, Iterable, Optional
+from toolbox.utils.decorator_factory import decorator
 
 
 def include_filter(ignore_values: bool = False):
 
-    def include_filter(base_filter: Callable[..., str]) -> Callable[..., str]:
-
-        def include_filter_inner(
-            filter_prefix: str,
-            col: str,
-            values: Optional[Iterable] = None,
-            values_converter: Optional[Callable[[Any], str]] = None,
-        ):
-            if values or ignore_values:
-                filter_prefix = _try_add_space(filter_prefix)
-                return filter_prefix + base_filter(**locals())
-            return ''
-
-        return include_filter_inner
-
-    return include_filter
-
-
-def exclude_filter(base_filter: Callable[..., str]) -> Callable[..., str]:
-
-    def exclude_filter_inner(
+    @decorator
+    def include_filter_inner(
+        base_filter: Callable[..., str],
         filter_prefix: str,
         col: str,
         values: Optional[Iterable] = None,
         values_converter: Optional[Callable[[Any], str]] = None,
     ):
-        is_null_fitler = f'{col} IS NULL'
-        filter_prefix = _try_add_space(filter_prefix)
-        if values:
-            return f'{filter_prefix}({is_null_fitler} OR {base_filter(**locals())})'
-        return filter_prefix + is_null_fitler
+        if values or ignore_values:
+            filter_prefix = _try_add_space(filter_prefix)
+            return filter_prefix + base_filter(**locals())
+        return ''
 
-    return exclude_filter_inner
+    return include_filter_inner
+
+
+@decorator
+def exclude_filter(
+    base_filter: Callable[..., str],
+    filter_prefix: str,
+    col: str,
+    values: Optional[Iterable] = None,
+    values_converter: Optional[Callable[[Any], str]] = None,
+):
+    isnull_filter = f'{col} IS NULL'
+    filter_prefix = _try_add_space(filter_prefix)
+    if values:
+        return f'{filter_prefix}({isnull_filter} OR {base_filter(**locals())})'
+    return filter_prefix + isnull_filter
 
 
 def _try_add_space(prefix):
     if prefix:
         return prefix + ' '
     return prefix
-
-
-def in_values(values: Iterable, values_converter: Callable[[Any], str]):
-    return ','.join([values_converter(val) for val in values])
-
-
-def like(col: str, values: Iterable):
-    return ' OR '.join([f"{col} LIKE '%{value}%'" for value in values])
 
 
 @include_filter()
@@ -85,3 +74,11 @@ def generate_not_like_filter(col: str, values: Iterable, **_):
 @include_filter(ignore_values=True)
 def generate_is_not_null_filter(col: str, **_) -> str:
     return f'{col} IS NOT NULL'
+
+
+def in_values(values: Iterable, values_converter: Callable[[Any], str]):
+    return ','.join([values_converter(val) for val in values])
+
+
+def like(col: str, values: Iterable):
+    return ' OR '.join([f"{col} LIKE '%{value}%'" for value in values])
---
+++ utils/decorator_factory.py
@@ -0,0 +1,72 @@
+import functools
+
+
+class FuncProxy:
+    """
+    Class which looks and behaves like what it wraps.
+    """
+
+    def __init__(self, func):
+        self.func = func
+        try:
+            self.__name__ = func.__name__
+        except AttributeError:
+            pass
+
+    @property
+    def __class__(self):
+        return self.func.__class__
+
+    def __getattr__(self, name):
+        return getattr(self.func, name)
+
+
+class BoundFuncWrapper(FuncProxy):
+
+    def __init__(self, func, wrapper):
+        FuncProxy.__init__(self, func)
+        self.wrapper = wrapper
+
+    def __call__(self, *args, **kwargs):
+        return self.wrapper(self.func, *args, **kwargs)
+
+
+class FuncWrapper(FuncProxy):
+
+    def __init__(self, func, wrapper):
+        FuncProxy.__init__(self, func)
+        self.wrapper = wrapper
+
+    def __get__(self, instance, owner):
+        """
+        Class function objects are actually descriptors.
+        When you access that function using a dotted attribute path,
+        you are invoking the __get__() method to bind the function to the class instance,
+        turning it into a bound method of that object.
+        So when calling a method of a class, it is not the __call__() method of the original function object that is called,
+        but the __call__() method of the temporary bound object that is created as a result of accessing the function.
+
+        This method is aimed to solve the problem where the wrapper is not honouring the descriptor protocol
+        and performing binding on the wrapped object in the case of a method on a class.
+        Thus wrappers for class method need also to be descriptors.
+        """
+        func = self.func.__get__(instance, owner)
+        return BoundFuncWrapper(func, self.wrapper)
+
+    def __call__(self, *args, **kwargs):
+        """
+        If the wrapper is applied to a normal function, the __call__() method of the wrapper is used.
+        """
+        return self.wrapper(self.func, *args, **kwargs)
+
+
+def decorator(wrapper):
+    """
+    A decorator to help us create our own specialized decorators.
+    """
+
+    @functools.wraps(wrapper)
+    def _decorator(func):
+        return FuncWrapper(func, wrapper)
+
+    return _decorator
