7519ebbec23aa9ab9d44b78555bc2766051c9739
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-03-10 22:04:48 +0400

    refactor

--- sql/query_executors/sql_query_executor.py
+++ sql/query_executors/sql_query_executor.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import Any, Callable, Dict, Iterable, Union, Optional
+from typing import Any, Dict, Iterable, Union, Optional
+from wrapt import decorator
 from pandas import DataFrame, read_sql
 from toolbox.sql.sql_query import SqlQuery
 from toolbox.sql.connections.connection import (
@@ -9,33 +11,23 @@ from toolbox.sql.connections.connection import (
 from toolbox.logger import Logger
 
 
+@decorator
+def with_transaction(execute_query_func, instance: SqlQueryExecutor, args, kwargs):
+    conn = instance.get_connection_object()
+    with conn.begin_transaction() as transaction:
+        return execute_query_func(*args, **kwargs, conn=transaction)
+
+
 class SqlQueryExecutor(ABC):
 
     def execute_script_non_query(
         self,
         script: str,
     ) -> None:
+        self._execute_script(script=script)
 
-        def func(conn: Transaction):
-            self._execute_script(
-                script=script,
-                conn=conn,
-            )
-
-        self._execute_query_func(func=func)
-
-    def execute_non_query(
-        self,
-        sql_query: SqlQuery,
-    ) -> None:
-
-        def func(conn: Transaction):
-            self._execute_non_query(
-                sql_query=sql_query,
-                conn=conn,
-            )
-
-        self._execute_query_func(func=func)
+    def execute_non_query(self, sql_query: SqlQuery) -> None:
+        self._execute_non_query(sql_query=sql_query)
 
     def execute_many(
         self,
@@ -43,17 +35,12 @@ class SqlQueryExecutor(ABC):
         main_query: SqlQuery,
         main_query_read_kwargs: Dict[str, Any] = {},
     ) -> DataFrame:
-
-        def func(conn: Transaction):
-            self._execute_prep_queries(prep_queries, conn)
-            Logger.debug(main_query._query_file_path)
-            return self._execute_query(
-                sql_query=main_query,
-                connection=conn,
-                kwargs=main_query_read_kwargs,
-            )
-
-        return self._execute_query_func(func=func)
+        self._execute_prep_queries(prep_queries)
+        Logger.debug(main_query._query_file_path)
+        return self._execute_query(
+            sql_query=main_query,
+            kwargs=main_query_read_kwargs,
+        )
 
     def execute_many_main_queries(
         self,
@@ -61,37 +48,34 @@ class SqlQueryExecutor(ABC):
         main_queries: Dict[str, SqlQuery],
         main_query_read_kwargs: Dict[str, Any] = {},
     ) -> Dict[str, DataFrame]:
+        self._execute_prep_queries(prep_queries)
+        res = {}
+        for k, v in main_queries.items():
+            Logger.debug(f'{k} : {v._query_file_path}')
+            res[k] = self._execute_query(
+                sql_query=v,
+                kwargs=main_query_read_kwargs,
+            )
+        return res
 
-        def func(conn: Transaction):
-            self._execute_prep_queries(prep_queries, conn)
-            res = {}
-            for k, v in main_queries.items():
-                Logger.debug(f'{k} : {v._query_file_path}')
-                res[k] = self._execute_query(
-                    sql_query=v,
-                    connection=conn,
-                    kwargs=main_query_read_kwargs,
-                )
-            return res
-
-        return self._execute_query_func(func=func)
-
+    @with_transaction
     def _execute_query(
         self,
         sql_query: SqlQuery,
-        connection: Transaction,
         kwargs: Dict[str, Any],
+        conn: Optional[Transaction] = None,
     ) -> Union[DataFrame, str]:
         return read_sql(
             sql=sql_query.get_query(),
-            con=connection,
+            con=conn,
             **kwargs,
         )
 
+    @with_transaction
     def _execute_prep_queries(
         self,
         prep_queries: Iterable[SqlQuery],
-        conn: Transaction,
+        conn: Optional[Transaction] = None,
     ):
         for sql_query in prep_queries:
             Logger.debug(sql_query._query_file_path)
@@ -100,20 +84,22 @@ class SqlQueryExecutor(ABC):
                 conn=conn,
             )
 
+    @with_transaction
     def _execute_non_query(
         self,
         sql_query: SqlQuery,
-        conn: Transaction,
+        conn: Optional[Transaction] = None,
     ):
         self._execute_script(
             script=sql_query.get_query(),
             conn=conn,
         )
 
+    @with_transaction
     def _execute_script(
         self,
         script: str,
-        conn: Transaction,
+        conn: Optional[Transaction] = None,
     ):
         execute = conn.execute
         if hasattr(conn, 'executescript'):
@@ -124,13 +110,6 @@ class SqlQueryExecutor(ABC):
     def get_connection_object(self) -> Connection:
         pass
 
-    def _begin_transaction(self):
-        return self.get_connection_object().begin_transaction()
-
-    def _execute_query_func(self, func: Callable[[Transaction], Any]):
-        with self._begin_transaction() as transaction:
-            return func(transaction)
-
 
 class SqlNonQueryExecutor(SqlQueryExecutor):
 
@@ -140,19 +119,13 @@ class SqlNonQueryExecutor(SqlQueryExecutor):
         main_query: Optional[SqlQuery] = None,
         main_query_kwargs: Optional[Dict[str, Any]] = None,
     ) -> None:
-
-        def func(conn: Transaction):
-            self._execute_prep_queries(
-                prep_queries=prep_queries,
-                conn=conn,
-            )
-
-        self._execute_query_func(func=func)
+        self._execute_prep_queries(prep_queries)
 
     def execute_many_main_queries(
         self,
         prep_queries: Iterable[SqlQuery],
         main_queries: Dict[str, SqlQuery],
         main_query_read_kwargs: Dict[str, Any] = {},
+        conn: Optional[Transaction] = None,
     ) -> Dict[str, Union[DataFrame, str]]:
         raise NotImplementedError()
