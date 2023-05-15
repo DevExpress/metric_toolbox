bb915b50513f3da9731d2deca17471d04d1f18ba
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-05-15 10:51:30 +0400

    update

--- utils/network.py
+++ utils/network.py
@@ -5,45 +5,42 @@ import urllib3
 urllib3.disable_warnings()
 
 
-class Network:
+def get_data(
+    end_point: str,
+    headers={},
+    params={},
+) -> str:
+    resp = requests.get(
+        url=end_point,
+        headers=headers,
+        params=params,
+        verify=False,
+    )
+    _raise_if_status_code_indicates_failure(
+        end_point=end_point,
+        status_code=resp.status_code,
+        success_status_code=200,
+    )
+    return resp.text
 
-    @staticmethod
-    def get_data(
-        end_point: str,
-        headers={},
-        params={},
-    ) -> str:
-        resp = requests.get(
-            url=end_point,
-            headers=headers,
-            params=params,
-            verify=False,
-        )
-        Network._raise_if_status_code_indicates_failure(
-            end_point=end_point,
-            status_code=resp.status_code,
-            success_status_code=200,
-        )
-        return resp.text
 
-    @staticmethod
-    def post_data(end_point: str, data: str):
-        resp = requests.post(url=end_point, json=data)
-        Network._raise_if_status_code_indicates_failure(
-            end_point=end_point,
-            status_code=resp.status_code,
-            success_status_code=201,
-        )
-        return resp.status_code
+def post_data(end_point: str, data: str):
+    resp = requests.post(url=end_point, json=data)
+    _raise_if_status_code_indicates_failure(
+        end_point=end_point,
+        status_code=resp.status_code,
+        success_status_code=201,
+    )
+    return resp.status_code
 
-    @staticmethod
-    def _raise_if_status_code_indicates_failure(
-        end_point: str,
-        status_code: int,
-        success_status_code: int = 200,
-    ):
-        if status_code != success_status_code:
-            raise RequestFailure(end_point)
+
+def _raise_if_status_code_indicates_failure(
+    end_point: str,
+    status_code: int,
+    success_status_code: int = 200,
+):
+    if status_code != success_status_code:
+        raise RequestFailure(end_point)
 
 
 class RequestFailure(Exception):
