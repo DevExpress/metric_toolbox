d185e969f98b431dbe1953f03f95382399b2765e
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-07-13 17:34:04 +0400

    support groups

--- sql/aggs/__init__.py
+++ sql/aggs/__init__.py
@@ -2,4 +2,4 @@ from toolbox.sql.aggs.metrics import Metric
 from toolbox.sql.aggs.functions import SUM, COUNT, COUNT_DISTINCT
 
 
-none_metric = Metric('Fake', SUM(0))
+NONE_METRIC = Metric('Fake', 'Empty', SUM(0))
--- sql/aggs/metrics.py
+++ sql/aggs/metrics.py
@@ -4,26 +4,27 @@ from toolbox.sql.aggs.functions import Func
 
 class Metric(NamedTuple):
     name: str
+    group: str
     expression: Func
 
     def __str__(self) -> str:
         return str(self.expression)
 
     def __add__(self, other: 'Metric'):
-        return Metric('', self.expression + other.expression)
+        return Metric('', '', self.expression + other.expression)
 
     def __mul__(self, other: 'Metric'):
-        return Metric('', self.expression * other.expression)
+        return Metric('', '', self.expression * other.expression)
 
     def __truediv__(self, other: 'Metric'):
-        return Metric('', self.expression / other.expression)
+        return Metric('', '', self.expression / other.expression)
 
     def __eq__(self, other: 'Metric') -> bool:
         return str(self) == str(other)
 
     @classmethod
-    def from_metric(cls, name: str, metric: 'Metric'):
-        return cls(name, metric.expression)
+    def from_metric(cls, name: str, group: str, metric: 'Metric') -> 'Metric':
+        return cls(name, group, metric.expression)
 
     def get_over(self, window: str) -> str:
         return self.expression.over(window)
