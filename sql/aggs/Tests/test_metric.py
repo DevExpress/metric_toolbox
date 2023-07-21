3e922f5861c3570820e48eb0ec13df0d914d019d
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-07-21 15:31:11 +0400

    add display name and description

--- sql/aggs/Tests/test_metric.py
+++ sql/aggs/Tests/test_metric.py
@@ -5,19 +5,19 @@ from toolbox.sql.aggs.metrics import Metric
 
 # yapf: disable
 def test_str():
-    assert str(Metric('', '', 'expr1')) == 'expr1'
+    assert str(Metric('', '', '', '', 'expr1')) == 'expr1'
 
 def test_eq():
-    assert Metric('', '', SUM('expr1')) == Metric('', '', SUM('expr1'))
+    assert Metric('', '', '', '', SUM('expr1')) == Metric('', '', '', '', SUM('expr1'))
 
 def test_add():
-    assert Metric('', '', SUM('expr1')) + Metric('', '', SUM('expr2')) == Metric('', '', SUM('expr1') + SUM('expr2'))
+    assert Metric('', '', '', '', SUM('expr1')) + Metric('', '', '', '', SUM('expr2')) == Metric('', '', '', '', SUM('expr1') + SUM('expr2'))
 
 def test_mul():
-    assert Metric('', '', SUM('expr1')) * Metric('', '', SUM('expr2')) == Metric('', '', SUM('expr1') * SUM('expr2'))
+    assert Metric('', '', '', '', SUM('expr1')) * Metric('', '', '', '', SUM('expr2')) == Metric('', '', '', '', SUM('expr1') * SUM('expr2'))
 
 def test_div():
-    assert Metric('', '', SUM('expr1')) / Metric('', '', SUM('expr2')) == Metric('', '', SUM('expr1') / SUM('expr2'))
+    assert Metric('', '', '', '', SUM('expr1')) / Metric('', '', '', '', SUM('expr2')) == Metric('', '', '', '', SUM('expr1') / SUM('expr2'))
 
 @pytest.mark.parametrize(
     'func, window, res', [
@@ -49,12 +49,12 @@ def test_div():
     ]
 )
 def test_over_simple(func, window, res):
-    assert Metric('tst', '', func).get_over(window) == res
+    assert Metric('tst', '', '', '', func).get_over(window) == res
 
 def test_over():
-    m1 = Metric('', '', SUM('qwe'))
-    m2 = Metric('', '', SUM('asd'))
-    m3 = Metric('', '', SUM('zxc'))
-    metric = Metric.from_metric('tst', '', (m1 + m2) / m3)
+    m1 = Metric('', '', '', '', SUM('qwe'))
+    m2 = Metric('', '', '', '', SUM('asd'))
+    m3 = Metric('', '', '', '', SUM('zxc'))
+    metric = Metric.from_metric('tst', '', '', '', (m1 + m2) / m3)
     res = 'IIF(SUM(zxc) OVER (wnd) = 0, 0, (SUM(qwe) OVER (wnd) + SUM(asd) OVER (wnd)) * 1.0 / SUM(zxc) OVER (wnd))'
     assert metric.get_over('wnd') == res
--- sql/aggs/metrics.py
+++ sql/aggs/metrics.py
@@ -4,6 +4,8 @@ from toolbox.sql.aggs.functions import Func
 
 class Metric(NamedTuple):
     name: str
+    displayName: str
+    description: str
     group: str
     expression: Func
 
@@ -11,20 +13,27 @@ class Metric(NamedTuple):
         return str(self.expression)
 
     def __add__(self, other: 'Metric'):
-        return Metric('', '', self.expression + other.expression)
+        return Metric('', '', '', '', self.expression + other.expression)
 
     def __mul__(self, other: 'Metric'):
-        return Metric('', '', self.expression * other.expression)
+        return Metric('', '', '', '', self.expression * other.expression)
 
     def __truediv__(self, other: 'Metric'):
-        return Metric('', '', self.expression / other.expression)
+        return Metric('', '', '', '', self.expression / other.expression)
 
     def __eq__(self, other: 'Metric') -> bool:
         return str(self) == str(other)
 
     @classmethod
-    def from_metric(cls, name: str, group: str, metric: 'Metric') -> 'Metric':
-        return cls(name, group, metric.expression)
+    def from_metric(
+        cls,
+        name: str,
+        displayName: str,
+        description: str,
+        group: str,
+        metric: 'Metric',
+    ) -> 'Metric':
+        return cls(name, displayName, description, group, metric.expression)
 
     def get_over(self, window: str) -> str:
         return self.expression.over(window)
