92a5130fedcc6050ed971a68e89cf237420f11ce
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-07-21 15:40:34 +0400

    keep display name only

--- sql/aggs/__init__.py
+++ sql/aggs/__init__.py
@@ -2,4 +2,4 @@ from toolbox.sql.aggs.metrics import Metric
 from toolbox.sql.aggs.functions import SUM, COUNT, COUNT_DISTINCT
 
 
-NONE_METRIC = Metric('Fake', '', '', 'Empty', SUM(0))
+NONE_METRIC = Metric('Fake', '', 'Empty', SUM(0))
