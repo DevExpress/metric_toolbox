850fcd0f11f676f3c176ef1987968ba5c97750e9
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-07-31 11:37:23 +0400

    fix possible sqlite parser stack overflow

--- utils/sqlite_query_formatters.py
+++ utils/sqlite_query_formatters.py
@@ -1,20 +1,34 @@
 from collections.abc import Sequence
 import itertools
+import math
 
 
 def json_array_of_objects(fields: Sequence[str], raw_query: str):
-    SQLITE_MAX_FUNCTION_ARG = 63
-    cols = ',\n\t'.join(f"'{v}', {v}" for v in itertools.islice(fields, 0, SQLITE_MAX_FUNCTION_ARG))
-    json_obj_fn = f'JSON_OBJECT(\n\t{cols})'
-    for v in itertools.islice(fields, SQLITE_MAX_FUNCTION_ARG, None):
-        json_obj_fn = f"JSON_INSERT(\n\t{json_obj_fn},\n\t '$.{v}', {v})"
     return f"""
 SELECT  JSON_GROUP_ARRAY(
-        {json_obj_fn}
+        {_json_object(fields)}
     ) AS json_result
 FROM (
 {raw_query}
 )"""
+
+
+# yapf: disable
+def _json_object(fields: Sequence[str]) -> tuple[int, str]:
+    SQLITE_MAX_FUNCTION_ARG = 63
+    parts = math.ceil(len(fields) / SQLITE_MAX_FUNCTION_ARG)
+
+    def gen():
+        for i in range(parts):
+            start = i * SQLITE_MAX_FUNCTION_ARG
+            cols = ',\n\t'.join(f"'{v}', {v}" for v in itertools.islice(fields, start, start + SQLITE_MAX_FUNCTION_ARG))
+            yield f'JSON_OBJECT(\n\t{cols})'
+
+    json_obj_fn = ',\n\t'.join(gen())
+    if parts > 1:
+        return f'JSON_PATCH(\n\t{json_obj_fn})'
+    return json_obj_fn
+# yapf: enable
 
 
 def json_array_of_values(fields: Sequence[str], raw_query: str):
