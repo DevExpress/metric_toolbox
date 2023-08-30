9c96f76122fef03d1b822d4037e49c4f9955335a
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-08-30 21:35:06 +0400

    add recalculate_for_last_n_months

--- utils/env.py
+++ utils/env.py
@@ -1,9 +1,23 @@
 import os
 
 
+__recalc_from_beginning = 'RECALCULATE_FROM_THE_BEGINNING'
+
+
 def recalculate_from_beginning():
-    return int(os.environ['RECALCULATE_FROM_THE_BEGINNING']) == 1
+    return int(os.environ[__recalc_from_beginning]) == 1
 
 
 def reset_recalculate_from_beginning():
-    os.environ['RECALCULATE_FROM_THE_BEGINNING'] = str(0)
+    os.environ[__recalc_from_beginning] = str(0)
+
+
+__recalc_for_last_months = 'RECALCULATE_FOR_LAST_MONTHS'
+
+
+def recalculate_for_last_n_months() -> int:
+    return int(os.environ.get(__recalc_for_last_months, 0))
+
+
+def reset_recalculate_for_last_months():
+    os.environ[__recalc_for_last_months] = str(0)
