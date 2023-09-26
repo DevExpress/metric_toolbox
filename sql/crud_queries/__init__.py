db7c574612883fb24f749b03662cfeac3fcc538b
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-09-26 14:44:50 +0400

    import SqliteCreateTableQuery

--- sql/crud_queries/__init__.py
+++ sql/crud_queries/__init__.py
@@ -1,2 +1,6 @@
 from toolbox.sql.crud_queries.upsert import SqliteUpsertQuery
-from toolbox.sql.crud_queries.create import SqliteCreateTableFromTableQuery, QueryField
+from toolbox.sql.crud_queries.create import (
+    SqliteCreateTableQuery,
+    SqliteCreateTableFromTableQuery,
+    QueryField,
+)
