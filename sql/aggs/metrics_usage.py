cf570ced1b751a0ad8b390c6cbe0f72013d2fe99
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2024-03-25 16:52:03 +0400

    add metrics usage count

---
+++ sql/aggs/metrics_usage.py
@@ -0,0 +1,50 @@
+from prometheus_client import Counter
+from toolbox.sql.aggs import Metric
+from wrapt import decorator
+from collections.abc import Awaitable, Iterable, Mapping, Callable
+
+
+__metrics_usage = None
+
+
+def __ensure_metrics_usage(
+    metrics: Callable[[], Iterable[Metric]],
+    subsystem: str,
+):
+    global __metrics_usage
+    if __metrics_usage is None:
+
+        def normilize(name: str) -> str:
+            return "".join(filter(str.isalnum, name))
+
+        def counter(metric: Metric) -> Counter:
+            name = metric.get_display_name()
+            return Counter(
+                name=normilize(name),
+                documentation=f'represents {name} metric usage.',
+                subsystem=subsystem,
+            )
+
+        __metrics_usage = {m.name: counter(m) for m in metrics()}
+
+
+def track_metric_usage(
+    metrics: Callable[[], Iterable[Metric]],
+    subsystem: str,
+):
+    __ensure_metrics_usage(metrics, subsystem)
+
+    @decorator
+    async def usage(
+        func: Awaitable,
+        instance,
+        args,
+        kwargs: Mapping,
+    ) -> Awaitable:
+        metric = kwargs.get('metric')
+        usage = __metrics_usage.get(metric)
+        if usage:
+            usage.inc()
+        return await func(*args, **kwargs)
+
+    return usage
