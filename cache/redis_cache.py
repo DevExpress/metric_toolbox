8a24f451df01f18b33e842febce9fb06d7cfa2b1
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2024-04-05 15:27:25 +0400

    redis cache: lazy create instance

--- cache/redis_cache.py
+++ cache/redis_cache.py
@@ -4,15 +4,22 @@ from typing import Any, Optional
 from toolbox.cache.utils import get_key as _get_key
 
 
-__instance = redis.Redis(
-    host=config.redis_service(),
-    port=config.redis_port(),
-    password='',
-    decode_responses=True,
-)
+__instance = None
+
+
+def __ensure_instance():
+    global __instance
+    if __instance is None:
+        __instance = redis.Redis(
+            host=config.redis_service(),
+            port=config.redis_port(),
+            password='',
+            decode_responses=True,
+        )
 
 
 def get(key: str) -> str:
+    __ensure_instance()
     return __instance.get(key)
 
 
@@ -21,6 +28,7 @@ def set(
     value: Any,
     ex: Optional[int],
 ):
+    __ensure_instance()
     __instance.set(
         key,
         value,
